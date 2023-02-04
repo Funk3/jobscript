60b41a3f8da7f516aeafe69bf1edeef911f27047
Zak <zak.johansson@gmail.com>
2023-02-04 12:25:33 -0800

    change file

--- api/index.js
+++ api/index.js
@@ -2,16 +2,18 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
 const cors = require('cors');
+
+const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 const routes = require('./routes/routes');
 
 app.use('/', routes);
 
 app.listen(PORT, () => {
-  console.log(`AICV listening on port ${PORT}`);
+  console.log(`Jobscript Server is listening on port ${PORT}`);
 });

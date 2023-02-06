3c0c49244514d1a608e80f3d7e5959416c254ae5
Zak <zak.johansson@gmail.com>
2023-02-05 20:00:20 -0800

    add ai api beginnings

--- api/index.js
+++ api/index.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 const routes = require('./routes/routes');
+const ai_routes = require('./routes/ai_routes');
 
 app.use('/', routes);
+app.use('/airoute', ai_routes);
 
 app.listen(PORT, () => {
   console.log(`Jobscript Server is listening on port ${PORT}`);
---
+++ api/routes/ai_routes.js
@@ -0,0 +1,20 @@
+// load .env data into process.env
+require('dotenv').config();
+
+const express = require('express');
+const router = express.Router();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-002",
+  prompt: "Hello world",
+});
+console.log(completion.data.choices[0].text);
+
+module.exports = router;

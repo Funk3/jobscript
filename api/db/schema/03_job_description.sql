f279c4815321680ce2908a996f972a0a249fed1e
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-02-06 13:47:06 -0800

    add job_title and company_name

--- api/db/schema/03_job_description.sql
+++ api/db/schema/03_job_description.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS job_description CASCADE;
 CREATE TABLE job_description (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id),
+  job_title TEXT NOT NULL,
+  company_name TEXT NOT NULL,
   description TEXT NOT NULL,
   cover_letter_text TEXT ,
   keywords VARCHAR(25),

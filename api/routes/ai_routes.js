55d90c135c52eb5ab03a0673ca4cf8c894cf861c
Zak <zak.johansson@gmail.com>
2023-02-06 12:53:26 -0800

    create proper post for open ai api"

--- api/routes/ai_routes.js
+++ api/routes/ai_routes.js
@@ -3,34 +3,26 @@ const axios = require('axios');
 // load .env data into process.env
 require('dotenv').config();
 
-const express = require('express');
-const router = express.Router();
-
 const apiKey = process.env.OPENAI_API_KEY;
 
-router.post('/', (req, res) => {
-  const client = axios.create({
-    headers: {
-      Authorization: 'Bearer ' + apiKey,
-    },
-  });
-
-  const params = {
-    prompt: 'How are you?',
-    model: 'text-davinci-003',
-    max_tokens: 10,
-    temperature: 0,
-  };
-
-  client
-    .post('https://api.openai.com/v1/completions', params)
-    .then((result) => {
-      console.log(result.data.choices[0].text);
-      res.json(`${result.data.choices[0].text}`);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const client = axios.create({
+  headers: {
+    Authorization: 'Bearer ' + apiKey,
+  },
 });
 
-module.exports = router;
+const params = {
+  prompt: 'Write a cover letter with this',
+  model: 'text-davinci-003',
+  max_tokens: 2000,
+  temperature: 0,
+};
+
+client
+  .post('https://api.openai.com/v1/completions', params)
+  .then((result) => {
+    console.log(result.data.choices[0].text);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

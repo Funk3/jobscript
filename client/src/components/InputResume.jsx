61a6944ae2a2336cd63b3cb554d03ee1adf8f428
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-02-06 13:48:04 -0800

    add textarea/input within form tag

--- client/src/components/InputResume.jsx
+++ client/src/components/InputResume.jsx
@@ -5,9 +5,10 @@ export default function InputResume() {
     <section className="rhs-input-resume-section">
       <h3>Upload your CV here</h3>
       <button>Upload</button>
-      
       <div>
-        <textarea placeholder="Or Copy and paste the contents of your resume here"></textarea>
+       <form>
+        <textarea placeholder="Or Copy and paste the contents of your resume here" rows="5" cols="33"></textarea>
+       </form>
       </div>
     </section>
   );

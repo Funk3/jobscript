de8c3b6b2f355e8a85a6e1d6a590abf581b0964f
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-02-06 20:04:51 -0800

    remove header from landing page

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Footer() {
+  return (
+    <footer className="landing-page-footer">
+        <a>FAQ</a>
+        <a>Pricing</a>
+        <a>Contact</a>
+      </footer>
+  )
+
+}

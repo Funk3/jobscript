5538e37a8e440bf5257b350851355db07414924e
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-02-07 15:22:05 -0800

    start cover letter provider

--- client/src/components/CoverLetter.jsx
+++ client/src/components/CoverLetter.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function CoverLetter() {
-  //helper function (here or in app.js and pass as prop?) getCoverLetterByJobAppId(jobAppId) that takes in job_app id  and returns string with job_app.cover_letter_text
-  // returnedCoverLetterText = getCoverLetterByJobAppId(state.jobappid or props.jobappid?)
+
+const [coverLetterText, setCoverLetterText] = useState("Default cover letter text");
+
 
   //edit buttons might need to be their own components as they do different things? or helpers to direct onClick
   return (
     <div className="rhs-cover-letter">
       <form>
         <textarea
-          defaultValue="returnedCoverLetterText"
+          defaultValue={coverLetterText}
           rows="5"
           cols="33"
         ></textarea>
 
         <section className="manage-cl-buttons">
-          <button> Edit</button>
           <button> Save</button>
           <button> Regenerate</button>
           <button> Copy</button>

a5214555e8e6fa88dbcecf97d118985d68d78272
Zak <zak.johansson@gmail.com>
2023-02-07 16:04:22 -0800

    add body parser to enable axiost POST of body data

--- api/routes/userLogin.js
+++ api/routes/userLogin.js
@@ -6,6 +6,6 @@ const { getUserByEmail, addUser } = require('../db/queries/users');
 
 // Login with route
 router.post('/login', (req, res) => {
-  res.send(console.log(req.body));
+  res.send(getUserByEmail(req.body.email));
 });
 module.exports = router;
--- client/src/__helpers__/loginroute.js
+++ client/src/__helpers__/loginroute.js
@@ -1,25 +1,5 @@
 import axios from 'axios';
 
-// export default function login() {
-//   const options = {
-//     url: `${process.env.REACT_APP_SERVER_URL}/account/login`,
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json;charset=UTF-8',
-//     },
-//     data: {
-//       email: 'zak@zak.com',
-//     },
-//   };
-
-//   axios(options).then((response) => {
-//     console.log(response);
-//   });
-// }
-
-
-
 export default function login() {
   axios({
     method: 'post',

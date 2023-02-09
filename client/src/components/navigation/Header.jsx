1bae94d7be9c98b3a5713de66f390239912c7027
Ryan De Mesa <ryandemesa09@gmail.com>
2023-02-09 13:08:36 -0700

    Fix nav logo & button styling

--- client/src/components/navigation/Header.jsx
+++ client/src/components/navigation/Header.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 import { useAuthContext } from "providers/AuthProvider";
-import LogoutButton from 'components/buttons/LogoutButton';
-import LoginButton from 'components/buttons/LoginButton';
-
+import LogoutButton from "components/buttons/LogoutButton";
+import LoginButton from "components/buttons/LoginButton";
 
 export default function Header() {
-
   const { user } = useAuthContext();
 
   return (
-    <header className='main-header'>
-      <nav className='nav-bar'>
-        <img className='logo' src='../../JobScript.png' />
-        {!user && 
-        <>
-        <button className='log-btn'> Register </button>
-        <LoginButton/>
-        </>}
-        
-        {user && 
-        <>
-        Currently logged in as:{user.email}
-        <LogoutButton/>
-        </>}
-        
-        
+    <header className="main-header">
+      <nav className="nav-bar">
+        <img className="logo" src="../../JobScript.png" />
+        {!user && (
+          <>
+            <div className="nav-btns">
+              <button className="log-btn"> Register </button>
+            <LoginButton />
+            </div>
+          </>
+        )}
+
+        {user && (
+          <>
+            <p className="curr">Currently logged in as:{user.email}</p>
+            <LogoutButton />
+          </>
+        )}
       </nav>
     </header>
   );

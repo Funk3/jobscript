571239a7a0c4eb3ea5bac90590d39b59c1954cd1
Zak <zak.johansson@gmail.com>
2023-02-10 15:52:38 -0800

    save file adds date

--- client/src/components/buttons/Download.jsx
+++ client/src/components/buttons/Download.jsx
@@ -6,9 +6,15 @@ export default function Download() {
   const { coverLetterText } = useCoverLetterContext();
 
   const handleDownload = () => {
-    console.log('cover letter', coverLetterText);
-    let blob = new Blob([coverLetterText]);
-    saveAs(blob, 'coverletter');
+    const date = new Date();
+    let blob = new Blob(
+      [coverLetterText],
+      {
+        type: 'text/plain;charset=utf-8',
+      },
+      { autoBom: true }
+    );
+    saveAs(blob, `coverletter${date.toLocaleString()}`);
   };
 
   return <button onClick={handleDownload}>Download</button>;

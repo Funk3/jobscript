1a1d628d71ca0ea0cb11003b7020db09f817d913
Zak <zak.johansson@gmail.com>
2023-02-11 11:28:07 -0800

    add copy to clipboard button

--- client/src/components/CoverLetter.jsx
+++ client/src/components/CoverLetter.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Save from "./buttons/Save";
-import Download from "./buttons/Download";
-import { useCoverLetterContext } from "../providers/CoverLetterProvider";
+import React from 'react';
+import Save from './buttons/Save';
+import Download from './buttons/Download';
+import Copy from './buttons/Copy';
+import { useCoverLetterContext } from '../providers/CoverLetterProvider';
 
-export default function CoverLetter({fetchJobData}) {
+export default function CoverLetter({ fetchJobData }) {
   const { coverLetterText, handleChange } = useCoverLetterContext();
 
   const handleCoverLetterSubmit = (e) => {
@@ -11,22 +12,22 @@ export default function CoverLetter({fetchJobData}) {
   };
 
   return (
-    <div className="rhs-cover-letter">
+    <div className='rhs-cover-letter'>
       {coverLetterText ? (
         <>
           <h3> Your AI Cover Letter: </h3>
           <form onSubmit={handleCoverLetterSubmit}>
             <textarea
-              className="rhs-textarea"
+              className='rhs-textarea'
               value={coverLetterText}
               onChange={(event) => handleChange(event.target.value)}
-              rows="5"
-              cols="33"
+              rows='5'
+              cols='33'
             ></textarea>
 
-            <section className="manage-cl-buttons">
+            <section className='manage-cl-buttons'>
               <Save fetchJobData={fetchJobData} />
-              <button> Copy</button>
+              <Copy />
               <Download />
             </section>
           </form>
@@ -36,11 +37,11 @@ export default function CoverLetter({fetchJobData}) {
           <h3>Your Cover Letter will appear here:</h3>
           <form>
             <textarea
-              className="rhs-textarea"
-              value=""
+              className='rhs-textarea'
+              value=''
               onChange={(event) => handleChange(event.target.value)}
-              rows="5"
-              cols="33"
+              rows='5'
+              cols='33'
             ></textarea>
           </form>
         </>
